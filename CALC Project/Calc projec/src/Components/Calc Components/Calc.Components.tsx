689e91f4f51772ff5f8dc880220dc9156d9a423a
ZenatyAh <zenatyah@gmail.com>
2025-04-21 21:17:42 +0300

    using regular expression for analyse the operation and number

--- CALC Project/Calc projec/src/Components/Calc Components/Calc.Components.tsx
+++ CALC Project/Calc projec/src/Components/Calc Components/Calc.Components.tsx
@@ -5,8 +5,8 @@ const Calc = () => {
   const [num,setNum] = useState("");
   const InputHandler = (val : string ) => {
     if(val === '='){
-      console.log(num +val+ eval(num))
-      setNum(num +val+ eval(num));
+      // the new solution here
+      ParseOperation(num);
     }else if(val === 'C'){
       setNum('');
     }
@@ -20,10 +20,25 @@ const Calc = () => {
 
   return (
     <div>
-      <input type='text' value={num}/>
+      <input type='text' value={num} className='result'/>
       <Button InputHandler={InputHandler}/>
     </div>
   )
 };
 
 export default Calc;
+
+const ParseOperation = (val : string) => {
+  console.log(val);
+  for (let index = 0; index < val.length; index++) {
+  // using regular expression
+  if(/[0-9.]/.test(val[index])){
+    // numbers
+    console.log('number')
+  }else if(/[+\-*/]/.test(val[index])){
+    // operation
+    console.log('operation')
+  }
+
+  }
+}
